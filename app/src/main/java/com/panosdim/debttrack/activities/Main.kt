456dc2ed8c06edb289632098a36134c532bbe1c2
Panagiotis Dimopoulos <panosdim@gmail.com>
2023-07-04 16:26:50 +0300

    Add check for new version

--- app/src/main/java/com/panosdim/debttrack/activities/Main.kt
+++ app/src/main/java/com/panosdim/debttrack/activities/Main.kt
@@ -1,5 +1,10 @@
 package com.panosdim.debttrack.activities
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,11 +15,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.FirebaseApp
 import com.panosdim.debttrack.ui.theme.DebtTrackTheme
+import com.panosdim.debttrack.utils.checkForNewVersion
+import com.panosdim.debttrack.utils.refId
 
 class Main : ComponentActivity() {
+    private lateinit var manager: DownloadManager
+    private lateinit var onComplete: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Handle new version installation after the download of APK file.
+        manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        onComplete = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val referenceId = intent!!.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                if (referenceId != -1L && referenceId == refId) {
+                    val apkUri = manager.getUriForDownloadedFile(refId)
+                    val installIntent = Intent(Intent.ACTION_VIEW)
+                    installIntent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+                    installIntent.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    startActivity(installIntent)
+                }
+
+            }
+        }
+        registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        FirebaseApp.initializeApp(this)
+
+        // Check for new version
+        checkForNewVersion(this)
+
         setContent {
             DebtTrackTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/panosdim/debttrack/utils/Utils.kt
@@ -0,0 +1,83 @@
+package com.panosdim.debttrack.utils
+
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.pm.PackageInfoCompat
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import com.panosdim.debttrack.R
+import com.panosdim.debttrack.TAG
+import com.panosdim.debttrack.model.FileMetadata
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+var refId: Long = -1
+
+@Suppress("DEPRECATION")
+fun checkForNewVersion(context: Context) {
+    val storage = Firebase.storage
+    val metadataFileName = "output-metadata.json"
+    val apkFileName = "app-release.apk"
+
+    // Create a storage reference from our app
+    val storageRef = storage.reference
+
+    // Create a metadata reference
+    val metadataRef: StorageReference = storageRef.child(metadataFileName)
+
+    metadataRef.getBytes(Long.MAX_VALUE).addOnSuccessListener {
+        // Use the bytes to display the image
+        val data = String(it)
+        val fileMetadata = Json.decodeFromString<FileMetadata>(data)
+        val version = fileMetadata.elements[0].versionCode
+
+        val appVersion = PackageInfoCompat.getLongVersionCode(
+            context.packageManager.getPackageInfo(
+                context.packageName,
+                0
+            )
+        )
+
+        if (version > appVersion) {
+            Toast.makeText(
+                context,
+                context.getString(R.string.new_version),
+                Toast.LENGTH_LONG
+            ).show()
+
+            val versionName = fileMetadata.elements[0].versionName
+
+            // Create an apk reference
+            val apkRef = storageRef.child(apkFileName)
+
+            apkRef.downloadUrl.addOnSuccessListener { uri ->
+                downloadNewVersion(context, uri, versionName)
+            }.addOnFailureListener {
+                // Handle any errors
+                Log.w(TAG, "Fail to download file $apkFileName")
+            }
+        }
+    }.addOnFailureListener {
+        // Handle any errors
+        Log.w(TAG, "Fail to retrieve $metadataFileName")
+    }
+}
+
+private fun downloadNewVersion(context: Context, downloadUrl: Uri, version: String) {
+    val manager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+    val request =
+        DownloadManager.Request(downloadUrl)
+    request.setDescription("Downloading new version of DebtTrack.")
+    request.setTitle("New DebtTrack Version: $version")
+    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+    request.setDestinationInExternalPublicDir(
+        Environment.DIRECTORY_DOWNLOADS,
+        "DebtTrack-${version}.apk"
+    )
+    refId = manager.enqueue(request)
+}
